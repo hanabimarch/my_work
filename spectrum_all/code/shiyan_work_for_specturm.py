82cbf065d275adee8202b30d636f42d893770955
zoujinhang <653538607@qq.com>
2019-10-08 11:00:02 +0800

    更新光谱数据中间过程

--- spectrum_all/code/shiyan_work_for_specturm.py
+++ spectrum_all/code/shiyan_work_for_specturm.py
@@ -1,8 +1,8 @@
-from zjh_make_phaI import make_phaI
+from zjh_make_phaI import *
 import zzh_py3_file as zhf
 import os
 from zjh_xspec_fit_kernel import xspec_fit_kernel
-
+import matplotlib.pyplot as plt
 
 
 data_topdir = '/home/laojin/trigdata/2017/'
@@ -23,20 +23,20 @@ print('year name list:\n',yearnamelist)
 for bnname in yearnamelist:
 	file_1_link = result_topdir + bnname + '/good_NaI_detectors.txt'
 	file_2_link = result_topdir + bnname + '/good_BGO_detectors.txt'
-	file_3_link = result_topdir + bnname + '/pulse_edges.txt'
+	file_3_link = result_topdir + bnname + '/0_pulse_edges.txt'
+	save_all = result_topdir + 'A_spectrum_1/'
+	save_all2 = result_topdir + 'A_spectrum_2/'
+	if(os.path.exists(save_all) == False):
+		os.makedirs(save_all)
+	if (os.path.exists(save_all2) == False):
+		os.makedirs(save_all2)
 
 	if(os.path.exists(file_1_link) and os.path.exists(file_2_link) and os.path.exists(file_3_link)):
-		if(os.path.getsize(file_1_link) == 0 or os.path.getsize(file_2_link) == 0 or os.path.getsize(file_1_link) == 0 or os.path.getsize(file_3_link) == 0):
-			print(bnname+'探头数量不足')
-			continue
-			
-		print('---------------------------------------')
-		print(bnname)
+
 		ni_s,ni_list = zhf.readcol(file_1_link)
 		bgo = zhf.readcol(file_2_link)[0][0]
 
 		if(len(ni_s)>=2):
-
 			try:
 				ni_list = ni_list[:2]
 
@@ -49,19 +49,25 @@ for bnname in yearnamelist:
 					print('--------------------------------------')
 					print('time slice ',i)
 					savedir = makedirs + 'time_' + str(i) + '/'
-					filelist = ['A_slice_'+bnname+'_'+bgo+'.pha']
+					filelist = ['A_'+bnname+'_'+bgo+'.pha']
 					for ni in ni_list:
 						print(ni)
-						filelist.append('A_slice_'+bnname+'_'+ni+'.pha')
-						make_phaI(bnname,ni,data_topdir,savedir,edges_start[i],edges_stop[i])
+						filelist.append('A_'+bnname+'_'+ni+'.pha')
+						make_phaI_with_events(bnname,ni,data_topdir,savedir,edges_start[i],edges_stop[i])
+						plt.savefig(save_all+'A_'+bnname+'_'+ni+'_'+str(i)+'.png')
+						plt.close()
 					print(bgo)
-					make_phaI(bnname,bgo,data_topdir,savedir,edges_start[i],edges_stop[i])
+					make_phaI_with_events(bnname,bgo,data_topdir,savedir,edges_start[i],edges_stop[i])
+					plt.savefig(save_all+'A_'+bnname+'_'+bgo+'_'+str(i)+'.png')
+					plt.close()
 					print('---------------------------------------')
 					try:
 						value,value_arr1,value_arr2,flux_list = xspec_fit_kernel(filelist,savedir,makedirs+'Z_'+str(i))
+						#plt.savefig(save_all2 + 'A_'+bnname+'_'+str(i)+'.png')
+						#plt.close()
+						copy_rspI(savedir+'foldedspec.png',save_all2 + 'A_'+bnname+'_'+str(i)+'.png')
 						zhf.printdatatofile(makedirs+'Z_'+str(i)+'_Flux.txt',data = flux_list)
 						zhf.printdatatofile(makedirs+'Z_'+str(i)+'_Epec.txt',data = [[value],[value_arr1],[value_arr2]])
-						
 					except:
 						print(bnname+' '+str(i)+'出现错误，跳过拟合！')
 						continue
